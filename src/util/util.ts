0406d609e7105d006928af806cd51e722d900c48
MateoGiuffra <matteogiuffrah40@gmail.com>
2024-12-07 23:32:59 -0300

    agrego el https

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL =  'ouijpers-deploy-production.up.railway.app';
+export const API_BASE_URL =  'https://ouijpers-deploy-production.up.railway.app';
 // export const API_BASE_URL = 'https://soothing-fascination-production.up.railway.app';
 // export const API_BASE_URL = 'http://localhost:8080';
